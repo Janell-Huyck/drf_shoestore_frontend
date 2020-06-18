f18a09046f70d0c5e4d4f47b57d6924769a8387f
Janell.Huyck <Janell.Huyck@outlook.com>
2020-06-18 18:44:00 -0400

    initial commit plus initial attempt at showing shoes

--- src/App.js
+++ src/App.js
@@ -2,25 +2,31 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      shoes: []
+    }
+  }
+  componentDidMount() {
+    fetch('http://127.0.0.1:8000/api/shoe/')
+      .then(res => res.json())
+      .then(data => console.log(data))
+  }
+  render() {
+    return (
+      <div>
+        <ul>
+          {this.state.shoes.map(shoe => {
+            return <li>{shoe.name}</li>
+          })}
+        </ul>
+      </div>
+    );
+  }
+
+
 }
 
 export default App;
