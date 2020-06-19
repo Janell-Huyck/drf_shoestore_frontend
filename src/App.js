e02a80f157d2ba00e6ebdc647e422b44238d3ec7
Janell.Huyck <Janell.Huyck@outlook.com>
2020-06-18 21:03:25 -0400

    formatted shoe display for readability

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends React.Component {
@@ -12,14 +11,26 @@ class App extends React.Component {
   componentDidMount() {
     fetch('http://127.0.0.1:8000/api/shoe/')
       .then(res => res.json())
-      .then(data => console.log(data))
+      .then(data => this.setState({ shoes: data }))
   }
   render() {
     return (
       <div>
         <ul>
           {this.state.shoes.map(shoe => {
-            return <li>{shoe.name}</li>
+            return (
+              <li key={shoe.id}>
+                -size: {shoe.size}<br></br>
+                -brand: {shoe.brand_name}<br></br>
+                -manufacturer: {shoe.manufacturer}<br></br>
+                -color: {shoe.color}<br></br>
+                -material: {shoe.material}<br></br>
+                -type: {shoe.shoe_type}<br></br>
+                -fastener: {shoe.fasten_type}<br></br>
+                <br></br>
+              </li>
+
+            )
           })}
         </ul>
       </div>
